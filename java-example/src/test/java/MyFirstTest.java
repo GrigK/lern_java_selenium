fde8751cf732b8b51c4f09591574bfa29be66474
FrendsTeamApp <FrendsTeamApp@gmail.com>
2019-11-01 17:21:14 +0300

    another webdriver m.b. Firefox

--- java-example/src/test/java/MyFirstTest.java
+++ java-example/src/test/java/MyFirstTest.java
@@ -3,8 +3,8 @@ import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -17,7 +17,7 @@ public class MyFirstTest {
 
     @Before
     public void start(){
-        driver= new ChromeDriver();
+        driver= new ChromeDriver(); //FirefoxDriver();
         // если не нашел то ждать 10 сек
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         wait= new WebDriverWait(driver, 10);
