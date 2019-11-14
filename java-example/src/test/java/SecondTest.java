52e783e7f7c99e30d1f9f43c77148049116bb6c2
FrendsTeamApp <FrendsTeamApp@gmail.com>
2019-11-14 16:56:01 +0300

    refactoring

--- java-example/src/test/java/SecondTest.java
+++ java-example/src/test/java/SecondTest.java
@@ -1,21 +1,26 @@
-import Locators.First;
 import org.junit.Test;
+import org.openqa.selenium.By;
 
 public class SecondTest extends TestBase {
+    public static final By VIRTUAL_KEYBOARD = By.cssSelector("div[aria-label=\"Экранная клавиатура\"]");
+    public static final By INPUT_FIELD = By.name("q");
+    public static final By BTN_OK = By.name("btnK");
+    public static final By KEY_SPACE = By.id("K32");
+
     @Test
     public void myGoogleTest() {
         setWait(2);
         driver.get("http://www.google.com/");
         // кликнем по виртуальной клавиатуре
-        assert (isElementPresent(First.VIRTUAL_KEYBOARD));
-        driver.findElement(First.VIRTUAL_KEYBOARD).click();
+        assert (isElementPresent(VIRTUAL_KEYBOARD));
+        driver.findElement(VIRTUAL_KEYBOARD).click();
         // на клавиатуре кликнем на пробел
-        assert (isElementPresent(First.KEY_SPACE)) : "Not found space key on virtual keyboard"; // надо подождать пока появится :)
-        driver.findElement(First.KEY_SPACE).click();
+        assert (isElementPresent(KEY_SPACE)) : "Not found space key on virtual keyboard"; // надо подождать пока появится :)
+        driver.findElement(KEY_SPACE).click();
         // закроем виртуальную клавиатуру
-        driver.findElement(First.VIRTUAL_KEYBOARD).click();
-        driver.findElement(First.INPUT_FIELD).sendKeys("webdriver"); // StaleElementReferenceException
-        driver.findElement(First.BTN_OK).click();
+        driver.findElement(VIRTUAL_KEYBOARD).click();
+        driver.findElement(INPUT_FIELD).sendKeys("webdriver"); // StaleElementReferenceException
+        driver.findElement(BTN_OK).click();
         assert expectTitlePage("webdriver - Поиск в Google") : "Incorrect page title ";
     }
 }
